2fc4fefc6f9321872551901af3c412e3000997a6
Joe Michael <jack_dexter@hotmail.com>
2024-09-21 05:45:03 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+import fastify from "fastify";
+
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+        messageFormat: "{msg} {req.method} {req.url}",
+        //hideObject: true,
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+const server = fastify({
+  logger: envToLogger["development"] ?? true,
+});
+
+server.get("/foo", async (request, reply) => {
+  reply.send({ msg: "foo" });
+});
+
+server.get("/bar", async (request, reply) => {
+  reply.send({ msg: "bar" });
+});
+
+//. https://fastify.dev/docs/latest/Reference/Server/
+// When deploying to a Docker, and potentially other, containers, it is advisable to listen on 0.0.0.0 because they do not default to exposing mapped ports to localhost:
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
